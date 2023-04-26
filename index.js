a61367571ba0f3178d52c5dbee0dbe34d2510ffe
Methusan105 <methusan@hotmail.no>
2023-04-26 20:51:47 +0200

    12

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 /* Blar gjennom hele HTML-dokumenter og finner elementet canvas og velger */
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector("canvas");
 
 /* Koden som henter 2d tegningskontekst fra html canvas elementet */
-const c = canvas.getContext('2d')
+const c = canvas.getContext("2d");
 /* Setter canvas sin lengde og bredde til 1024 og 576 */
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
 /* Koden som bruker fillrect metoden som bruker til å tegne en rektangel i canvas elementet. 
 Denne metoden tar inn 4 forskjellige argumenter som er: x, y, bredde og høyde.
 x og y er satt til 0 og bredden er satt til canvas sin lenge og bredde.*/
 
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
 
 /* Setter graviteten til 0.3 */
-const gravity = 0.3
+const gravity = 0.3;
 
 /* Denne koden oppretter en ny instans av klassen bakgrunn som er lagd i klasser js filen,
 og lagrer den i en variabel som er kalt for background.
@@ -25,10 +25,10 @@ Denne objektet inneholder egenskapen som posisjon og bildekilde.
 const background = new Bakgrunn({
   position: {
     x: 0,
-    y: 0
+    y: 0,
   },
-  imageSrc: './Tiny Forest Pack/paralax/forest_paralax.jpg'
-})
+  imageSrc: "./Tiny Forest Pack/paralax/forest_paralax.jpg",
+});
 
 /* Denne koden oppretter en ny instans av klassen bakgrunn som er lagd i klasser js filen,
 og lagrer den i en variabel som er kalt for shop.
@@ -38,12 +38,12 @@ Framesmax er klassen som angir maksimalt antall rammer i en animasjonssyklus. */
 const shop = new Bakgrunn({
   position: {
     x: 655,
-    y: 128
+    y: 128,
   },
-  imageSrc: './Tiny Forest Pack/shop.png',
+  imageSrc: "./Tiny Forest Pack/shop.png",
   scale: 3.15,
-  framesMax: 6
-})
+  framesMax: 6,
+});
 
 /* Denne koden oppretter en ny instans av klassen Spiller som er lagd i klasser js filen,
 og lagrer den i en variabel som er kalt for spiller1
@@ -54,28 +54,28 @@ const spiller1 = new Spiller({
   /* Setter startposisjonen til spiller1 x og y til 0 */
   position: {
     x: 0,
-    y: 0
+    y: 0,
   },
   /* Setter hastigheten til spiller1 x og y 0 */
   velocity: {
     x: 0,
-    y: 0
+    y: 0,
   },
   /* Setter forskyvning på spilleren x og y til 0 */
   offset: {
     x: 0,
-    y: 0
+    y: 0,
   },
   /* Setter bilden til spiller 1 til idle png
   Setter framesmax som er klassen til maksimalt antall rammer i en animasjonssyklus
   Setter bildeskalaen til 3.5
   Setter forskyvningen til spilleren x posisjonen til 100 og y posisjonen til 80 */
-  imageSrc: './Spiller 1/Idle.png',
+  imageSrc: "./Spiller 1/Idle.png",
   framesMax: 10,
   scale: 3.5,
   offset: {
     x: 100,
-    y: 80
+    y: 80,
   },
   /* Definerer et objekt med egenskapen sprites.
   Sprites inneholder masse forskjellige egenskaper som idle, run, jump, fall, attack1, takeHit og death
@@ -84,33 +84,33 @@ const spiller1 = new Spiller({
    */
   sprites: {
     idle: {
-      imageSrc: './Spiller 1/Idle.png',
-      framesMax: 10
+      imageSrc: "./Spiller 1/Idle.png",
+      framesMax: 10,
     },
     run: {
-      imageSrc: './Spiller 1/Run.png',
-      framesMax: 8
+      imageSrc: "./Spiller 1/Run.png",
+      framesMax: 8,
     },
     jump: {
-      imageSrc: './Spiller 1/Going Up.png',
-      framesMax: 3
+      imageSrc: "./Spiller 1/Going Up.png",
+      framesMax: 3,
     },
     fall: {
-      imageSrc: './Spiller 1/Going Down.png',
-      framesMax: 3
+      imageSrc: "./Spiller 1/Going Down.png",
+      framesMax: 3,
     },
     attack1: {
-      imageSrc: './Spiller 1/Attack1.png',
-      framesMax: 7
+      imageSrc: "./Spiller 1/Attack1.png",
+      framesMax: 7,
     },
     takeHit: {
-      imageSrc: './Spiller 1/Take Hit.png',
-      framesMax: 3
+      imageSrc: "./Spiller 1/Take Hit.png",
+      framesMax: 3,
     },
     death: {
-      imageSrc: './Spiller 1/Death.png',
-      framesMax: 11
-    }
+      imageSrc: "./Spiller 1/Death.png",
+      framesMax: 11,
+    },
   },
   /* Denne attackboxen inneholder et objekt som offset med størrelsen og plasseringen av en angrepsboks hos figuren
   Setter offset x verdien til 100 og y verdien til 50
@@ -118,12 +118,12 @@ const spiller1 = new Spiller({
   attackBox: {
     offset: {
       x: 100,
-      y: 50
+      y: 50,
     },
     width: 160,
-    height: 50
-  }
-})
+    height: 50,
+  },
+});
 
 /* Denne koden oppretter en ny instans av klassen Spiller som er lagd i klasser js filen,
 og lagrer den i en variabel som er kalt for spiller2
@@ -134,23 +134,23 @@ Denne egenskapen brukes til å justere posisjonen til spilleren på canvas eleme
 const spiller2 = new Spiller({
   position: {
     x: 400,
-    y: 100
+    y: 100,
   },
   velocity: {
     x: 0,
-    y: 0
+    y: 0,
   },
-  color: 'blue',
+  color: "blue",
   offset: {
     x: -50,
-    y: 0
+    y: 0,
   },
-  imageSrc: './Spiller 2/Idle.png',
+  imageSrc: "./Spiller 2/Idle.png",
   framesMax: 4,
   scale: 3.5,
   offset: {
     x: 215,
-    y: 250
+    y: 250,
   },
   /* Definerer et objekt med egenskapen sprites.
   Sprites inneholder masse forskjellige egenskaper som idle, run, jump, fall, attack1, takeHit og death
@@ -158,33 +158,33 @@ const spiller2 = new Spiller({
   Når idle blir tilkalt så viser den en annen idle bilden av spiller 2 og framesmax blir settet til 10 og det samme skjer med de andre */
   sprites: {
     idle: {
-      imageSrc: './Spiller 2/Idle.png',
-      framesMax: 4
+      imageSrc: "./Spiller 2/Idle.png",
+      framesMax: 4,
     },
     run: {
-      imageSrc: './Spiller 2/Run.png',
-      framesMax: 8
+      imageSrc: "./Spiller 2/Run.png",
+      framesMax: 8,
     },
     jump: {
-      imageSrc: './Spiller 2/Jump.png',
-      framesMax: 2
+      imageSrc: "./Spiller 2/Jump.png",
+      framesMax: 2,
     },
     fall: {
-      imageSrc: './Spiller 2/Fall.png',
-      framesMax: 2
+      imageSrc: "./Spiller 2/Fall.png",
+      framesMax: 2,
     },
     attack1: {
-      imageSrc: './Spiller 2/Attack1.png',
-      framesMax: 4
+      imageSrc: "./Spiller 2/Attack1.png",
+      framesMax: 4,
     },
     takeHit: {
-      imageSrc: './Spiller 2/Take hit.png',
-      framesMax: 3
+      imageSrc: "./Spiller 2/Take hit.png",
+      framesMax: 3,
     },
     death: {
-      imageSrc: './Spiller 2/Death.png',
-      framesMax: 7
-    }
+      imageSrc: "./Spiller 2/Death.png",
+      framesMax: 7,
+    },
   },
   /* Denne attackboxen inneholder et objekt som offset med størrelsen og plasseringen av en angrepsboks hos figuren
   Setter offset x verdien til -170 og y verdien til 50
@@ -192,12 +192,12 @@ const spiller2 = new Spiller({
   attackBox: {
     offset: {
       x: -170,
-      y: 50
+      y: 50,
     },
     width: 170,
-    height: 50
-  }
-})
+    height: 50,
+  },
+});
 
 /* Oppretter en konstant variabel som kalles for keys.
 Variabelen er et objekt som inneholder fire forskjellige egenskaper som er a, d, ArrowRight, ArrowLeft
@@ -205,108 +205,103 @@ Hver av de er også et objekt som en egenskap som er pressed og er satt til fals
 Denne koden brukes til å spore om bestemte tastetrykk er aktive eller ikke. */
 const keys = {
   a: {
-    pressed: false
+    pressed: false,
   },
   d: {
-    pressed: false
+    pressed: false,
   },
   ArrowRight: {
-    pressed: false
+    pressed: false,
   },
   ArrowLeft: {
-    pressed: false
-  }
-}
+    pressed: false,
+  },
+};
 
 /* Kjører funksjonen decreaseTimer */
-decreaseTimer()
+decreaseTimer();
 
 /* Lager en funksjon som heter animer.*/
 function animer() {
-
   /* Denne koden gjør til at funksjonen animer blir kalt igjen på neste animasjonsramme. */
-  window.requestAnimationFrame(animer)
+  window.requestAnimationFrame(animer);
 
   /* Setter fargestilen til svart */
-  c.fillStyle = 'black'
+  c.fillStyle = "black";
 
   /* Fyller den svarte fargen fra forrige koden på hele canvas elementet */
-  c.fillRect(0, 0, canvas.width, canvas.height)
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
   /* Tilkaller updatefunksjonen på konstante variabelen background,
   og tegner bakgrunnen hver gang funksjonen animer blir tilkalt */
-  background.update()
+  background.update();
 
   /* Tilkaller updatefunksjonen på konstante variabelen shop,
    og tegner bakgrunnen hver gang funksjonen snimer blir tilkalt */
-  shop.update()
+  shop.update();
 
   /* Denne koden fyller fargen hvit med gjennomsiktighet med 15%  */
-  c.fillStyle = 'rgba(255, 255, 255, 0.15)'
+  c.fillStyle = "rgba(255, 255, 255, 0.15)";
 
   /* Fyller den hvite fargen med 15% gjennomsiktighet fra forrige koden på hele canvas elementet */
-  c.fillRect(0, 0, canvas.width, canvas.height)
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
   /* Tilkaller updatefunksjonen på konstante variabelen spiller 1 og spiller 2,
   og blir tegnet i canvas elementet hver gang funksjonen animer blir tilkalt */
-  spiller1.update()
-  spiller2.update()
+  spiller1.update();
+  spiller2.update();
 
   /* Setter spiller 1 og spiller 2 sin x hastighet til 0 */
-  spiller1.velocity.x = 0
-  spiller2.velocity.x = 0
+  spiller1.velocity.x = 0;
+  spiller2.velocity.x = 0;
 
   /* Hvis knappen a er trykket og siste knappen som spiller 1 trykket er a,
   så setter den spiller 1 sin hastighet til -5 og  */
-  if (keys.a.pressed && spiller1.lastKey === 'a') {
-    spiller1.velocity.x = -5
+  if (keys.a.pressed && spiller1.lastKey === "a") {
+    spiller1.velocity.x = -5;
 
     /* Denne koden bytter fra idle bilden til løpe bilden */
-    spiller1.switchSprite('run')
+    spiller1.switchSprite("run");
     /* Ellers hvis knappen d og spilleren sin siste knapp er d,
     så skal spiller sin hastighet settes til -5 og bytte til løpe bilden */
-  } else if (keys.d.pressed && spiller1.lastKey === 'd') {
-    spiller1.velocity.x = 5
-    spiller1.switchSprite('run')
+  } else if (keys.d.pressed && spiller1.lastKey === "d") {
+    spiller1.velocity.x = 5;
+    spiller1.switchSprite("run");
 
     /* Ellers settes idle bilden som aktiv hvis ikke noen av knappene trykkes. */
   } else {
-    spiller1.switchSprite('idle')
+    spiller1.switchSprite("idle");
   }
 
   /* Hvis y hastigheten til spiller 1 er mer enn 0 (som angir at spilleren ikke er i bakken) så skal den bytte hoppe bilden til spiller 1 som den aktive bilden */
   if (spiller1.velocity.y < 0) {
-    spiller1.switchSprite('jump')
-  } 
-  /* Ellers hvis y hastigheten til spiller 1 mindre enn 0 (som angir at spilleren faller ned) så skal den bytte fra hoppe bilden til falle bilden */
-  else if (spiller1.velocity.y > 0) {
-    spiller1.switchSprite('fall')
+    spiller1.switchSprite("jump");
+  } else if (spiller1.velocity.y > 0) {
+    /* Ellers hvis y hastigheten til spiller 1 mindre enn 0 (som angir at spilleren faller ned) så skal den bytte fra hoppe bilden til falle bilden */
+    spiller1.switchSprite("fall");
   }
 
   /* Hvis knappen pil venstre er trykket og siste knappen som trykket er pil venstre,
   så skal den sette motstanderen sin x hastighet settes til -5 og skal bytte til løpe bilden til motstanderen */
-  if (keys.ArrowLeft.pressed && spiller2.lastKey === 'ArrowLeft') {
-    spiller2.velocity.x = -5
-    spiller2.switchSprite('run')    
-  } 
-  /* Ellers hvis knappen pil høyre er trykket og siste knappen som er trykket er pil høyre,
+  if (keys.ArrowLeft.pressed && spiller2.lastKey === "ArrowLeft") {
+    spiller2.velocity.x = -5;
+    spiller2.switchSprite("run");
+  } else if (keys.ArrowRight.pressed && spiller2.lastKey === "ArrowRight") {
+    /* Ellers hvis knappen pil høyre er trykket og siste knappen som er trykket er pil høyre,
   så skal motstanderen sin x hastighet settes til 5 og bytte til løpebilden til motstanderen */
-  else if (keys.ArrowRight.pressed && spiller2.lastKey === 'ArrowRight') {
-    spiller2.velocity.x = 5
-    spiller2.switchSprite('run')
-  } 
-  /* Ellers skal settes idle bilden som aktiv hvis ikke noen av knappene trykkes. */
-  else {
-    spiller2.switchSprite('idle')
+    spiller2.velocity.x = 5;
+    spiller2.switchSprite("run");
+  } else {
+    /* Ellers skal settes idle bilden som aktiv hvis ikke noen av knappene trykkes. */
+    spiller2.switchSprite("idle");
   }
 
-  /* Hvis y hastigheten til motstanderen er mer enn 0 (som angir at motstanderen ikke er i bakken) så skal den bytte hoppe bilden til spiller 1 som den aktive bilden */ 
+  /* Hvis y hastigheten til motstanderen er mer enn 0 (som angir at motstanderen ikke er i bakken) så skal den bytte hoppe bilden til spiller 1 som den aktive bilden */
   if (spiller2.velocity.y < 0) {
-    spiller2.switchSprite('jump')
-  } 
-  /* Ellers hvis y hastigheten til motstanderen mindre enn 0 (som angir at motstanderen faller ned) så skal den bytte fra hoppe bilden til falle bilden */
-  else if (spiller2.velocity.y > 0) {
-    spiller2.switchSprite('fall')
+    spiller2.switchSprite("jump");
+  } else if (spiller2.velocity.y > 0) {
+    /* Ellers hvis y hastigheten til motstanderen mindre enn 0 (som angir at motstanderen faller ned) så skal den bytte fra hoppe bilden til falle bilden */
+    spiller2.switchSprite("fall");
   }
 
   /* Denne koden sjekker om rektangelene mellom spiller 1 og motstanderen kolliderer
@@ -315,22 +310,22 @@ function animer() {
   Spiller 2 blir slått og mister liv, og setter spiller 1 ikke angriper,
   Den blar gjennom hele html dokumentet og velger iden enemyhealth og endrer bredden til motstanderen sin helse  */
   if (
-    rectangularCollision({
+    rektangelkollisjon({
       rectangle1: spiller1,
-      rectangle2: spiller2
+      rectangle2: spiller2,
     }) &&
-    spiller1.isAttacking &&
+    spiller1.angriper &&
     spiller1.framesCurrent === 4
   ) {
-    spiller2.takeHit()
-    spiller1.isAttacking = false
+    spiller2.takeHit();
+    spiller1.angriper = false;
     document.querySelector("#enemyhealth").style.width = spiller2.health + "%";
   }
 
   /* Hvis spiller 1 angriper og spiller 1 viser den fjerde bilden så skal den sette at spiller angriper til false 
   Denne koden skjer i tillegg når spiller 1 ikke treffer*/
-  if (spiller1.isAttacking && spiller1.framesCurrent === 4) {
-    spiller1.isAttacking = false
+  if (spiller1.angriper && spiller1.framesCurrent === 4) {
+    spiller1.angriper = false;
   }
 
   /* Denne koden sjekker om rektangelene mellom motstanderen og spiller 1  kolliderer
@@ -339,34 +334,34 @@ function animer() {
   Spiller 1 blir slått og mister liv, og setter spiller 2 ikke angriper,
   Den blar gjennom hele html dokumentet og velger iden playerhealth og endrer bredden til motstanderen sin helse  */
   if (
-    rectangularCollision({
+    rektangelkollisjon({
       rectangle1: spiller2,
-      rectangle2: spiller1
+      rectangle2: spiller1,
     }) &&
-    spiller2.isAttacking &&
+    spiller2.angriper &&
     spiller2.framesCurrent === 2
   ) {
-    spiller1.takeHit()
-    spiller2.isAttacking = false
-
+    spiller1.takeHit();
+    spiller2.angriper = false;
     document.querySelector("#playerhealth").style.width = spiller1.health + "%";
   }
 
   /* Hvis motstanderen angriper og motstanderen viser den andre bilden så skal den sette at spiller angriper til false 
   Denne koden skjer i tillegg når spiller 2 ikke treffer*/
-  if (spiller2.isAttacking && spiller2.framesCurrent === 2) {
-    spiller2.isAttacking = false
+  if (spiller2.angriper && spiller2.framesCurrent === 2) {
+    spiller2.angriper = false;
   }
 
   /* Hvis motstanderen sin helse er 0 eller hvis helsen til spiller 1,
    så skal den kjøre funksjonen determinewinner som tar spiller1, spiller2, timerid som argumenter  */
   if (spiller2.health <= 0 || spiller1.health <= 0) {
-    determineWinner({ spiller1, spiller2, timerId })
+    determineWinner({ spiller1, spiller2, timerId });
+    restartGame();
   }
 }
 
 /* Kjører funksjonen animer() */
-animer()
+animer();
 
 /* Koden lytter etter taster som blir trykket på,
 Den sjekker om spiller1 ikke er død (ved å sjekke om dead egenskapen er falsk),
@@ -375,71 +370,71 @@ Når knappen a trykkes på så setter den at knappen a ble trykket på til true 
 Når knappen w trykkes på setter den y hastighen til spiller1 til -11.5
 Når knappen mellomrom trykkes på så kalles attack funksjonen på spilleren
  */
-window.addEventListener('keydown', (event) => {
+window.addEventListener("keydown", (event) => {
   if (!spiller1.dead) {
     switch (event.key) {
-      case 'd':
-        keys.d.pressed = true
-        spiller1.lastKey = 'd'
-        break
-      case 'a':
-        keys.a.pressed = true
-        spiller1.lastKey = 'a'
-        break
-      case 'w':
-        spiller1.velocity.y = -11.5
-        break
-      case ' ':
-        spiller1.attack()
-        break
+      case "d":
+        keys.d.pressed = true;
+        spiller1.lastKey = "d";
+        break;
+      case "a":
+        keys.a.pressed = true;
+        spiller1.lastKey = "a";
+        break;
+      case "w":
+        spiller1.velocity.y = -11.5;
+        break;
+      case " ":
+        spiller1.attack();
+        break;
     }
   }
-/* Den sjekker om spiller2 ikke er død (ved å sjekke om dead egenskapen er falsk),
+  /* Den sjekker om spiller2 ikke er død (ved å sjekke om dead egenskapen er falsk),
 Når knappen pil høyre trykkes på så setter den at knappen pil høyre ble trykket på til true og spiller 1 trykket sist på pil høyre
 Når knappen pil venstre trykkes på så setter den at knappen pil venstre ble trykket på til true og spiller 1 trykket sist på pil venstre
 Når knappen pil opp trykkes på setter den y hastighen til spiller1 til -11.5
 Når knappen pil ned trykkes på så kalles attack funksjonen på spilleren */
   if (!spiller2.dead) {
     switch (event.key) {
-      case 'ArrowRight':
-        keys.ArrowRight.pressed = true
-        spiller2.lastKey = 'ArrowRight'
-        break
-      case 'ArrowLeft':
-        keys.ArrowLeft.pressed = true
-        spiller2.lastKey = 'ArrowLeft'
-        break
-      case 'ArrowUp':
-        spiller2.velocity.y = -11.5
-        break
-      case 'ArrowDown':
-        spiller2.attack()
-        break
+      case "ArrowRight":
+        keys.ArrowRight.pressed = true;
+        spiller2.lastKey = "ArrowRight";
+        break;
+      case "ArrowLeft":
+        keys.ArrowLeft.pressed = true;
+        spiller2.lastKey = "ArrowLeft";
+        break;
+      case "ArrowUp":
+        spiller2.velocity.y = -11.5;
+        break;
+      case "ArrowDown":
+        spiller2.attack();
+        break;
     }
   }
-})
+});
 /* Koden lytter etter taster som blir sluppet
 Når knappen d sluppes på så setter den at knappen d ble trykket på til false
 Når knappen a sluppes på så setter den at knappen a ble trykket på til false
  */
-window.addEventListener('keyup', (event) => {
+window.addEventListener("keyup", (event) => {
   switch (event.key) {
-    case 'd':
-      keys.d.pressed = false
-      break
-    case 'a':
-      keys.a.pressed = false
-      break
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
   }
 
-/* Når knappen Pil høyre sluppes på så setter den at knappen d ble trykket på til false
+  /* Når knappen Pil høyre sluppes på så setter den at knappen d ble trykket på til false
 Når knappen pil venstre sluppes på så setter den at knappen a ble trykket på til false */
   switch (event.key) {
-    case 'ArrowRight':
-      keys.ArrowRight.pressed = false
-      break
-    case 'ArrowLeft':
-      keys.ArrowLeft.pressed = false
-      break
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
   }
-})
+});
--- js/Funksjoner.js
+++ js/Funksjoner.js
@@ -1,4 +1,9 @@
-function rectangularCollision({ rectangle1, rectangle2 }) {
+/* Lager en funksjon som heter rektangelkollisjon og tar inn rectangle1 og rectangle2 som objekter
+ Denne funksjonen sjekker om begge disse rektangelene kolliderer eller overlapper hverandre
+ Den returnerer med true eller false om rektangelene kolliderer eller overlapper hverandre
+ Den sjekker om høyre kanten av rekangel 1 treffer venstre kanten av rektangel 2
+ Den sjekker om venstre kanten av rektangel 1 treffer høyre kanten av rektangel 2*/
+function rektangelkollisjon({ rectangle1, rectangle2 }) {
   return (
     rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
       rectangle2.position.x &&
@@ -7,31 +12,64 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
       rectangle2.position.y &&
     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  )
+  );
 }
-
+/* Lager en funksjon som heter determinewinner som tar inn spiller1, spiller2, timerid som objekter
+Den sletter tiden av variabelen timerid
+Den setter vinner til true
+Den blar gjennom hele dokumentet og leter etter iden displaytext og endrer egenskapene til elementets style objekt for å endre hvordan siden vises, den endrer display egenskapen til flex
+Hvis spiller1 sin helse og spiller2 sin helse er lik så,
+blar den gjennom hele html dokumentet og leter etter iden displaytext og skriver inn i den "uavgjort"
+Ellers hvis spiller1 sin helse er mer enn spiller2 sin helse så,
+blar den gjennom hele html dokumentet og leter etter iden displaytext og skriver inn i den "Spiller 1 vant"
+Ellers hvis spiller 2 sin helse er mer enn spiller1 sin helse så,
+blar den gjennom hele html dokumentet og letern etter iden displaytext og skriver inn i den "Spiller 2 vant"*/
 function determineWinner({ spiller1, spiller2, timerId }) {
-  clearTimeout(timerId)
-  document.querySelector('#displayText').style.display = 'flex'
+  clearTimeout(timerId);
+  vinner = true;
+  document.querySelector("#displayText").style.display = "flex";
   if (spiller1.health === spiller2.health) {
-    document.querySelector('#displayText').innerHTML = 'Tie'
+    document.querySelector("#displayText").innerHTML = "Uavgjort";
   } else if (spiller1.health > spiller2.health) {
-    document.querySelector('#displayText').innerHTML = 'Spiller 1 vant'
+    document.querySelector("#displayText").innerHTML = "Spiller 1 vant";
   } else if (spiller1.health < spiller2.health) {
-    document.querySelector('#displayText').innerHTML = 'Spiller 2 vant'
+    document.querySelector("#displayText").innerHTML = "Spiller 2 vant";
   }
 }
+/* lager variablen vinner og setter den til falsk,
+lager variablen timer og setter den på nummeret 60,
+lager variabelen timerId*/
+let vinner = false;
+let timer = 60;
+let timerId;
 
-let timer = 60
-let timerId
+/* Lager en funksjon som heter decreaseTimer
+Hvis timer er mer enn 0,
+så setter den timerId som slik at den tilkaller decreasetimer funksjonen hver eneste sekund,
+variabelen timer subraheres med 1
+den blar gjennom hele html dokumentet og velger iden timer og skriver i den nye variabelen timer
+hvis timer er 0,
+så skal den starte funksjonen determinewinner med spiller1, spiller2, timerid som objekter
+også skal den tilkalle funksjonen restartgame()*/
 function decreaseTimer() {
   if (timer > 0) {
-    timerId = setTimeout(decreaseTimer, 1000)
-    timer--
-    document.querySelector('#timer').innerHTML = timer
+    timerId = setTimeout(decreaseTimer, 1000);
+    timer--;
+    document.querySelector("#timer").innerHTML = timer;
   }
-
   if (timer === 0) {
-    determineWinner({ spiller1, spiller2, timerId })
+    determineWinner({ spiller1, spiller2, timerId });
+    restartGame();
+  }
+}
+
+/* Lager en funksjon som heter restart game
+Hvis vinner er true så setter den en timer på 5 sekunder,
+etter 5 sekunder har gått så vil den kjøre funksjonen location.reload() (som gjør til at siden lastes inn på nytt) */
+function restartGame() {
+  if (vinner) {
+    setTimeout(function () {
+      location.reload();
+    }, 5000);
   }
 }
